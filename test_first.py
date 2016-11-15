9c7e685cfbd46de8d548bf36221fc196fd1edb58
Kirill Berdyugin <kiryushah@gmail.com>
2016-11-15 19:31:07 +0400

    Update test_first.py

--- test_first.py
+++ test_first.py
@@ -1,1 +1,20 @@
-#_*_ coding: utf-8 _*_
+# -*- coding: utf-8 -*-
+import unittest
+from selenium import webdriver
+
+
+class VkComLogIn(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path="C://chromedriver/chromedriver.exe") #В скобках указываем путь к chromedriver.exe
+        self.driver.maximize_window()
+
+    def test_LogIn(self):
+        self.driver.get("http://www.vk.com")
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+    
