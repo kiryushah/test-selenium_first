6f985984548256d3cd18737324034de4310a87e2
Kirill Berdyugin <kiryushah@gmail.com>
2016-11-28 23:32:08 +0400

    Update regular_sale.py

--- regular_sale.py
+++ regular_sale.py
@@ -2,26 +2,27 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
-
-class regularsale(unittest.TestCase):
+class regular_sale(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("C://chromedriver/chromedriver.exe") #В скобках указываем путь к chromedriver.exe
         self.driver.maximize_window()
-        wait = WebDriverWait(self.driver, 10)
+        self.driver.implicitly_wait(20)
 
 
-    def test_regularsale(self):
+    def test_regular_sale(self):
         self.driver.get("http://localhost/litecart/")
     
         yellow = self.driver.find_element_by_xpath("//div[@id='box-campaigns']")
         duck = yellow.find_element_by_xpath("//a[@title='Yellow Duck']").click()
-        regular_text = self.driver.find_element_by_xpath("//s[@class='regular-price']").value_of_css_property('text-decoration')
+        yellow_duck = self.driver.find_element_by_xpath("//h1[text()='Yellow Duck']")
+        regular_text = self.driver.find_element_by_xpath("//s[text()='$20']").value_of_css_property('text-decoration')
         print(regular_text)
         regular_font = self.driver.find_element_by_xpath("//s[@class='regular-price']").value_of_css_property('font-size')
         print(regular_font)
-        campaign_weight = self.driver.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('font-weight')
+        campaign_weight = self.driver.find_element_by_xpath("//strong[text()='$18']").value_of_css_property('font-weight')
         print(campaign_weight)
         campaign_size = self.driver.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('font-size')
         print(campaign_size)
@@ -41,6 +42,9 @@ class regularsale(unittest.TestCase):
             print('true campaign font-size')
         else:
             print('false')
+        
+            
+
                 
     def tearDown(self):
         self.driver.close()
